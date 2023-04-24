7240de40d2ff2312413424c43eb29245686cf784
tkv1l <tkv1l@kali>
2023-04-25 00:55:54 +0200

    FEAT: redux and authorization

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import "./Register.scss";
 
 const Register = () => {
@@ -40,6 +40,76 @@ const Register = () => {
         <button type="submit">Registrarse</button>
       </form>
     </div>
+  );
+};
+
+export default Register; */
+
+import React from "react";
+import "./Register.scss";
+import { useForm } from "react-hook-form";
+import { newUser } from "../../redux/auth/auth.actions";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Login from "../Login/Login";
+
+const Register = ({ showLogin, setShowLogin }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const sendRegister = async (formData) => {
+    dispatch(newUser(formData, navigate));
+    setShowLogin(true);
+  };
+
+  const goToLogin = () => {
+    setShowLogin(true);
+  };
+  return (
+    <>
+      {!showLogin ? (
+        <div className="register-container">
+          <form onSubmit={handleSubmit(sendRegister)} className="register">
+            <h2>Register</h2>
+            <div>
+              <input
+                type="text"
+                placeholder="User name"
+                {...register("userName")}
+                name="userName"
+              />
+            </div>
+            <div>
+              <input
+                type="email"
+                placeholder="User email"
+                {...register("email")}
+                name="email"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                {...register("password")}
+                name="password"
+              />
+            </div>
+            <button>Register</button>
+          </form>
+          <div className="go-to-login-button">
+            <p>or</p>
+            <button onClick={goToLogin}>Login</button>
+          </div>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </>
   );
 };
 
